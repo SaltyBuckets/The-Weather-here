7215a1256ac002837d95b9cc49006230329d120c
SaltyBuckets <sanchitverma78@gmail.com>
2019-07-05 23:25:32 +0530

    refactoring

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-var express = require('express');
-var app = express();
-var port = 8080;
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const fetch = require('node-fetch');
+
+const port = process.env.PORT || 8080;
+
 app.listen(port, () => console.log(`Server listening on port ${port}`));
-// app.use('/', (req, res) => { res.send("this is my webpage"); })
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+app.post('/api', (req, res) => {
+    lat = req.body.lat;
+    lon = req.body.lon;
+    res.json({
+    })
+});
+
+app.get('/api/:latlon', async (req, res) => {
+    const url = 'http://api.openweathermap.org/data/2.5/weather?';
+    const api_key = process.env.API_KEY;
+    let lat, lon;
+    const latlon = req.params.latlon.split(',');
+    lat = latlon[0];
+    lon = latlon[1];
+    const newurl = url + `lat=${lat}&lon=${lon}&appid=${api_key}`;
+
+    try {
+        const response = await fetch(newurl, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+        const data = await response.json();
+
+        res.send(data);
+    } catch (err) { console.err(err); }
+});
--- public/sketch.js
+++ public/sketch.js
@@ -1,43 +1,41 @@
-let url = 'http://api.openweathermap.org/data/2.5/weather?';
-let api_key = 'process.env.API_KEY';
+
 let lat, lon;
 let units = 'metric';
 let data;
 let iconCode;
 let iconUrl;
-async function fetchAsync(newurl) {
-    let response = await fetch(newurl);
-    let datajson = await response.json();
-    data = await datajson;
-    await console.log(data);
-    await updateElements();
-}
 
 function success(pos) {
-    console.log(pos);
+    const lat = pos.coords.latitude;
+    const lon = pos.coords.longitude;
+    console.log("latitude:", lat, " longitude:", lon);
+    getWeatherData({ 'lat': lat, 'lon': lon })
+}
 
-    lat = pos.coords.latitude;
-    lon = pos.coords.longitude;
-    console.log(lat, lon);
-    var newurl = url + `lat=${lat}&lon=${lon}&appid=${api_key}&units=${units}`;
-    console.log(newurl);
-    fetchAsync(newurl);
+async function getWeatherData(coords) {
+    const options = {
+        method: 'GET',
+    }
+    const response = await fetch(`/api/${coords.lat},${coords.lon}`, options
+    );
+    const json = await response.json();
+    console.log("currentWeatherData:", json);
+    response.ok ? await updateElements(json) : Promise.reject(json);
 
 }
 
-function updateElements() {
-    document.getElementById('temp').textContent = data.main.temp;
+function updateElements(data) {
     document.getElementById('place').textContent = data.name;
-    document.getElementById('lat').textContent = lat;
-    document.getElementById('lon').textContent = lon;
+    document.getElementById('lat').textContent = data.coord.lat;
+    document.getElementById('lon').textContent = data.coord.lon;
     document.getElementById('weather').textContent = data.weather[0].description;
-    let temp_units;
-    if (units == 'metric') temp_units = 'C'
-    else if (units == 'imperial') temp_units = 'F'
-    else temp_units = 'K';
-    document.getElementById('temp').textContent = data.main.temp;
-    iconCode = data.weather[0].icon;
-    iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+    document.getElementById('temp').textContent = Math.round(data.main.temp) + ' K';
+    document.getElementById("kelvin").onclick = function () { document.getElementById('temp').textContent = Math.round(data.main.temp) + ' K'; };
+    document.getElementById("celsius").onclick = function () { document.getElementById('temp').textContent = Math.round(data.main.temp - 273.15) + ' °C'; };
+    document.getElementById("fahren").onclick = function () { document.getElementById('temp').textContent = Math.round(data.main.temp - 273.15) * 9 / 5 + 32 + ' °F'; };
+    const iconCode = data.weather[0].icon;
+    const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
     document.getElementById("icon").src = iconUrl;
 }
 
